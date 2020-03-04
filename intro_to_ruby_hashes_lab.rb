75a8f14f7b6c544ea4816c629b7d231f3149b862
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-04 22:22:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   	my_dogs = {:railroads => {}}
-  }
+  
 end
 
 def monopoly_with_third_tier
