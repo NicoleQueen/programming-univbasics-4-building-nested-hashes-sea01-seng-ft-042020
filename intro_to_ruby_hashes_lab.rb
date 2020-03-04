3c2b84714b8f39ef1870ebbb51e33b0550b8b638
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-04 22:32:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   # complexity.
   my_dogs = { 
     :railroads => {:pieces => 4, :rent_in_dollars => {
-    :1 => "", :2 => "", :3 => "", :4 => ""
+    :a => "", :b => "", :c => "", :d => ""
   }}}
 end
 
